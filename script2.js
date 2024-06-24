2deee834cf8bda060177325879408517a3e2ddd4
Deepak Behera <dpk6760@gmail.com>
2024-06-24 12:12:34 +0530

    second commit

---
+++ script2.js
@@ -0,0 +1,22 @@
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+
+// console.log(z);
+
+// {
+//     var man='Deeps'
+//     let name='Deepak';
+//     let isStudent=false;
+    
+//     console.log(name);
+//     console.log(typeof name);
+// }
+// console.log(name);
+// console.log(man);
+
+var n='I am a Developer';
+
+function pp(){
+    console.log(n);
+}
