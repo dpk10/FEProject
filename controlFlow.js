8b65c04a73597980c79836662c19a7efa48e9bbf
Deepak Behera <dpk6760@gmail.com>
2024-06-30 22:35:34 +0530

    sixth Commit

---
+++ controlFlow.js
@@ -0,0 +1,14 @@
+let man=17;
+
+if (man<18){
+    console.log("He is not an adult");
+}
+
+else if (man>18){
+
+    console.log("He is an adult");
+}
+
+else {
+    console.log("Neither he is less than or greater than 18 years old...")
+}
