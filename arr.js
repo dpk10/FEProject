65ab7aa7536d9515cc414affa5385aac28319f14
Deepak Behera <dpk6760@gmail.com>
2024-06-30 22:30:26 +0530

    Third commit

---
+++ arr.js
@@ -0,0 +1,20 @@
+let arr = [1,2,3,4,5,6,6,7,7,8]
+
+
+//let pushingElement = arr.pop();
+
+//console.log(arr)
+
+//console.log(pushingElement);
+
+//let slicedElements=arr.slice(-3);
+
+//console.log(slicedElements);
+
+const unknown=arr.map(counter)
+
+function counter(num){
+    return num * 10;
+}
+
+console.log(unknown);
---
+++ switch.js
@@ -0,0 +1,35 @@
+// let day=2;
+// let dayName;
+
+// switch(day){
+//     case 1: 
+//     dayName="Sunday"
+//     break;
+
+//     case 2:
+//         dayName="Monday"
+//     break;
+
+//     case 3:
+//         dayName="Tuesday"
+//     break;
+// }
+// console.log(dayName);
+
+
+let grade ='B';
+let result;
+switch(grade){
+    case 'A':
+        result="A (Excellent)";
+        break;
+    case 'B':
+        result="B (Average)";
+        break;
+    case 'c':
+        result="C (Below than Avarage";
+        break;
+    default:
+        result="No grade";
+}
+console.log(result);
