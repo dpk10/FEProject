83245c173a159b3c585b05d75e26e987bb03f0b9
Deepak Behera <dpk6760@gmail.com>
2024-06-30 22:32:44 +0530

    fourth commit

---
+++ arr1.js
@@ -0,0 +1,38 @@
+
+//let arr = [1,2,3,4,5,6,6,7,7,8]
+// let arr = [1,2,3,4,5,6,6,7,7,8]
+// let arr1 = ['himanshu','sooraj','raj','deepak','xyz']
+
+
+//let pushingElement = arr.pop();
+// // let pushingElement = arr.pop();
+
+//console.log(arr)
+// const slicedElements = arr.slice(-3) // 1st element indicates to the begin index  and 2nd elements indicates to the endIndex
+
+//console.log(pushingElement)
+// console.log(slicedElements)
+
+// // console.log(pushingElement)
+
+
+// const text = 'Hello, World'
+
+// const slicedText = text.slice(-7)
+
+// console.log(slicedText)
+
+
+let testingMap = new Map();
+
+testingMap.set('name','anonymous')
+testingMap.set('age',27)
+
+console.log(testingMap.get('name'))
+console.log(testingMap.get('age'))
+
+//testingMap.delete('age')
+
+console.log(testingMap.has('age'))
+
+console.log(testingMap.size)
