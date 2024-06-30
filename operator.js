3fc9b4131445d3c6ed63884f52d1723d98e3a632
Deepak Behera <dpk6760@gmail.com>
2024-06-30 23:09:58 +0530

    JavaScript Operator

---
+++ operator.js
@@ -0,0 +1,25 @@
+let a=100;
+let b=20;
+//let c=a+b;
+// let c=a*b;
+// let c=a/b;
+// let c=a-b;
+// let c=a<b;
+// let c=a>b;
+// let c=a<=b;
+// let c=(a+b)*5;
+// let c=(a+b)/b;
+// a+=5
+// a=+5
+// a*=5
+// a**=5;
+// a ^=5;
+// a |=5;
+// a &&=5;
+// a ||=5;
+a ??=5;
+
+console.log(a);
+// console.log(c);
+
+
