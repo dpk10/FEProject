e8219a47fdf7cb09ad77e9ff827fdd189477d118
Deepak Behera <dpk6760@gmail.com>
2024-07-02 23:10:14 +0530

    Ternary operator

---
+++ ternary.js
@@ -0,0 +1,31 @@
+// let age=27;
+
+// let ser=(age%2===0) ?'Even' : 'odd'; // To check even or odd by using ternary operator
+
+// console.log(ser);
+
+// let num=203;
+
+// let count=num%2
+
+// console.log(count);
+
+// let x= "I am global scoped";
+
+// function wow(){
+//     let x="I am block scoped"
+//     console.log(x);
+// }
+// wow()
+// console.log(x);
+
+function add(a,b){  
+    return a+b;
+}
+console.log(add(5,6));
+
+
+const name='Deepak';
+const age=27;
+
+console.log(`My name is ${name} and my age is ${age}`);
