35b624e91559450fbd538abca9da3df486709c22
Deepak Behera <dpk6760@gmail.com>
2024-07-10 20:12:50 +0530

    object creation

---
+++ object.js
@@ -0,0 +1,11 @@
+const a={
+    name: "Deepak",
+    age: 26,
+    section: 1,
+    place: "Odisha"
+}
+
+a["color"]="black"
+a['name']="Deeps"// here name has changed
+console.log(a);
+console.log(a.name);
