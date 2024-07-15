d713aec8607be5ca594dbdfcf63d058a200cb674
Deepak Behera <dpk6760@gmail.com>
2024-07-15 15:52:29 +0530

    For loop,For in and for of

---
+++ loop1.js
@@ -0,0 +1,52 @@
+// const arr = ['Naveen', "deepak", 'Dheerendra', 'Raj', 'Sachin', 'Rohit']
+
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//  console.log(element)   
+// }
+
+// const foo = 'Deeps'
+
+// for (let index = 0; index < 5; index++) {
+//     const element = foo;
+//     console.log(element)
+// }
+
+const foo = 'Deeps'
+
+for (let index = 0; index <foo.length; index++) {
+    const element = foo;
+    console.log(element)
+}
+
+
+// for of loop
+
+// let star = [1,2,3,4,5,5,6,6,6,7]
+
+// for ( element of star) {
+
+//     console.log(element)
+    
+// }
+
+//We can iterate through each item in an array by using for of loop
+
+// const counter = [1,2,5,7,8];
+// for ( i of counter) {
+//     console.log(counter[2])
+    
+// }
+
+//For in loop
+
+// let person = {
+//     name: "John",
+//     age: 30
+// };
+
+// for (let key in person) {
+//     console.log(key + ": " + person[key]);
+// }
+
+
