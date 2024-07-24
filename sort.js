1431892100819bdd65c4f09e493b1d7603edcb7b
Deepak Behera <dpk6760@gmail.com>
2024-07-24 23:06:25 +0530

    use of sort function

---
+++ sort.js
@@ -0,0 +1,9 @@
+let arr = [10,21,3,4,15,61,7,8]
+
+arr.sort((a,b) =>a-b)
+
+// console.log(arr);
+
+const months = ['April', 'June', 'Feb', 'Dec'];
+months.sort();
+console.log(months);// sorted in alphabetically order
